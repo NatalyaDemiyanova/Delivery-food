bf558485cdd652e1b480fd2aadf2c56a8d4e7987
NatalyaDemiyanova <natalya.demiyanova@gmail.com>
2020-04-10 20:50:02 +0300

    1

--- js/main.js
+++ js/main.js
@@ -10,15 +10,3 @@ function toggleModal() {
 }
 
 new WOW().init();
-
-// const buttonCart = document.querySelector('#button-shopping-cart');
-// const modal = document.querySelector('.modal');
-// const modalClose = document.querySelector('.modal__close');
-
-// buttonCart.addEventListener('click', function (event) {
-//     modal.classList.add('modal-active');
-// });
-
-// modalClose.addEventListener('click', function (event) {
-//     modal.classList.remove('modal-active');
-// });
